3ed5699cb46cbb518f7a47e51952601616bd2794
marcel.aubry <aubryma@bu.edu>
2022-10-09 17:55:40 -0400

    finished example of botometer

--- Project2/Project2_Botometer.py
+++ Project2/Project2_Botometer.py
@@ -3,13 +3,17 @@
 import botometer
 
 
+
 bom = botometer.Botometer(wait_on_ratelimit=True,
                           rapidapi_key=rapidapi_key,
                           **twitter_app_auth)
 
 # Check a single account by screen name
-result = bom.check_account('@clayadavis')
-print(result)
+accounts = ['@hello__marcel', '@aksel_laudon', '@AndreweTrapp', '@KylieJenner', '@AthleticsCanada']
+for screen_name, result in bom.check_accounts_in(accounts):
+  print(screen_name, result["display_scores"]["universal"])
+  if float(result["cap"]["universal"]) > 0.8:
+    print("This account is most likely a bot!!!")
 
 # # Check a single account by id
 # result = bom.check_account(1548959833)
