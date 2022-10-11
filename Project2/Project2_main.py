b9b0fe6324e7d61ba39ca3fad04e54440edb3451
marcel.aubry <aubryma@bu.edu>
2022-10-11 17:29:59 -0400

    connected both APIs

--- Project2/Project2_main.py
+++ Project2/Project2_main.py
@@ -1,7 +1,5 @@
 # EC601 PROJECT2
 # Copyright Marcel Aubry 2022
-# Collecting tweets from Twitter API v2
-# Source: https://towardsdatascience.com/an-extensive-guide-to-collecting-tweets-from-twitter-api-v2-for-academic-research-using-python-3-518fcb71df2a
 
 # For sending GET requests from the API
 import requests
@@ -9,6 +7,7 @@ import requests
 import os
 # For dealing with json responses we receive from the API
 import json
+import botometer
 
 
 def auth():
@@ -18,11 +17,12 @@ def create_headers(bearer_token):
     headers = {"Authorization": "Bearer {}".format(bearer_token)}
     return headers
 
-crypto = "bitcoin"
+user_input = input("Enter the cryptocurrency you are interested in!\n")
 
+maxresults = 10
 def create_url():
-    query = "query=%23{} lang:en".format(crypto)
-    max_results = "max_results=12"
+    query = "query=%23{} lang:en".format(user_input)
+    max_results = "max_results={}".format(maxresults)
     tweet_fields = "tweet.fields=created_at,public_metrics,attachments,text"
     expansions = "expansions=attachments.media_keys,author_id"
     user_fields = "user.fields=id,name,username,description"
@@ -37,13 +37,34 @@ def connect_to_endpoint(url, headers):
         raise Exception(response.status_code, response.text)
     return response.json()
 
+bom = botometer.Botometer(wait_on_ratelimit=True,
+                          rapidapi_key=rapidapi_key,
+                          **twitter_app_auth)
+
+def use_botometer(json_response):  
+    for i in range(maxresults):
+        user_id = int(json.dumps(json_response["data"][i]["author_id"], indent=4, sort_keys=True)[1:-1])
+        result = bom.check_account(user_id) 
+        print(result["display_scores"]["universal"])
+        if float(result["cap"]["universal"]) > 0.8:
+            print("This account is most likely a bot!!!")
+
 def main():
     bearer_token = auth()
     url = create_url()
     headers = create_headers(bearer_token)
     json_response = connect_to_endpoint(url, headers)
-    print(json.dumps(json_response["data"][3], indent=4, sort_keys=True))
-
+    use_botometer(json_response)
+    #print(json.dumps(json_response, indent=4, sort_keys=True))
+    
 
 if __name__ == "__main__":
     main()
+
+# # Check a single account by id
+# result = bom.check_account(1548959833)
+
+# # Check a sequence of accounts
+# accounts = ['@clayadavis', '@onurvarol', '@jabawack']
+# for screen_name, result in bom.check_accounts_in(accounts):
+#     # Do stuff with `screen_name` and `result`
