65fd929d44b46ecb7ba9d79f72c74b85051a6624
marcel.aubry <aubryma@bu.edu>
2022-10-16 18:52:36 -0400

    finished main

--- Project2/Project2_main.py
+++ Project2/Project2_main.py
@@ -8,6 +8,7 @@ import os
 # For dealing with json responses we receive from the API
 import json
 import botometer
+from google.cloud import language_v1
 
 
 def auth():
@@ -19,7 +20,7 @@ def create_headers(bearer_token):
 
 user_input = input("Enter the cryptocurrency you are interested in!\n")
 
-maxresults = 10
+maxresults = 20
 def create_url():
     query = "query=%23{} lang:en".format(user_input)
     max_results = "max_results={}".format(maxresults)
@@ -37,17 +38,44 @@ def connect_to_endpoint(url, headers):
         raise Exception(response.status_code, response.text)
     return response.json()
 
+
 bom = botometer.Botometer(wait_on_ratelimit=True,
                           rapidapi_key=rapidapi_key,
                           **twitter_app_auth)
 
 def use_botometer(json_response):  
+    count = 0
+    sentiment_count = 0
+    sentiment_score = 0
     for i in range(maxresults):
         user_id = int(json.dumps(json_response["data"][i]["author_id"], indent=4, sort_keys=True)[1:-1])
-        result = bom.check_account(user_id) 
-        print(result["display_scores"]["universal"])
-        if float(result["cap"]["universal"]) > 0.8:
-            print("This account is most likely a bot!!!")
+        result = bom.check_account(user_id)
+        if float(result["cap"]["universal"]) > 0.85:
+            count += 1
+        else:
+            sentiment_count += 1
+            sentiment_score += analyze_sentiment(json.dumps(json_response["data"][i]["text"], indent=4, sort_keys=True))
+    avg_sent = sentiment_score/sentiment_count
+    perc_bot = count/maxresults*100
+
+    print(f'{perc_bot}% of account are most likely bots. \nOut of the {100-perc_bot}% real accounts that tweeted, their average sentiment score is: {avg_sent}')
+
+def analyze_sentiment(text_content):
+    
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "C:/Users/marce/Downloads/project2-365114-4a2eb989b280.json"
+
+    client = language_v1.LanguageServiceClient()
+
+    type_ = language_v1.Document.Type.PLAIN_TEXT
+
+    language = "en"
+    document = {"content": text_content, "type_": type_, "language": language}
+
+    encoding_type = language_v1.EncodingType.UTF8
+
+    response = client.analyze_sentiment(request = {'document': document, 'encoding_type': encoding_type})
+    return response.document_sentiment.score
+
 
 def main():
     bearer_token = auth()
@@ -55,16 +83,7 @@ def main():
     headers = create_headers(bearer_token)
     json_response = connect_to_endpoint(url, headers)
     use_botometer(json_response)
-    #print(json.dumps(json_response, indent=4, sort_keys=True))
-    
+
 
 if __name__ == "__main__":
     main()
-
-# # Check a single account by id
-# result = bom.check_account(1548959833)
-
-# # Check a sequence of accounts
-# accounts = ['@clayadavis', '@onurvarol', '@jabawack']
-# for screen_name, result in bom.check_accounts_in(accounts):
-#     # Do stuff with `screen_name` and `result`
